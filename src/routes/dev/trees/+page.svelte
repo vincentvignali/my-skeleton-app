ed227bcb956596dddd3e12e2828071414bb6a67a
Vincent Vignali <vincent.vignali@syd.fr>
2023-08-08 18:33:36 +0200

    format and rename form to from

--- src/routes/dev/trees/+page.svelte
+++ src/routes/dev/trees/+page.svelte
@@ -66,26 +66,57 @@
 <div class="mx-auto pt-10 w-3/4">
 	<div class="rounded border-2 border-primary-500 p-2">
 		<h3 class="text-primary-500 text-center font-schoolBell text-lg p-1">
-			Data form Load Function
+			Data from Load Function
 		</h3>
-		<p class='text-primary-500 text-lg'>Within this table, the data is retrieved directly from the dedicated server page.</p>
+		<p class="text-primary-500 text-lg">
+			Within this table, the data is retrieved directly from the dedicated server page.
+		</p>
 		<ul class="text-primary-500 m-6">
-			<li class="text-primary-500 m-2"> - First, the current url got hit : <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">{$page.url}</span>.</li>
-			<li class="text-primary-500 m-2"> - Then, The <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">`+page.server.ts` (or`+layout.server.ts`)</span> load the data though the Load function.</li>
-			<li class="text-primary-500 m-2"> - Finally, The <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">`+page.svelte`</span> receives it within the <span class="bg-success-200 text-success-800 font-schoolBell rounded p-1">`export let data`</span> standard object.</li>
+			<li class="text-primary-500 m-2">
+				- First, the current url got hit : <span
+					class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">{$page.url}</span
+				>.
+			</li>
+			<li class="text-primary-500 m-2">
+				- Then, The <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1"
+					>`+page.server.ts` (or`+layout.server.ts`)</span> load the data though the Load function.
+			</li>
+			<li class="text-primary-500 m-2">
+				- Finally, The <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1"
+					>`+page.svelte`</span>
+				receives it within the
+				<span class="bg-success-200 text-success-800 font-schoolBell rounded p-1"
+					>`export let data`</span> standard object.
+			</li>
 		</ul>
 		<Table source={createTreeTableSource(treesFromLoadFunctions)} color="table-compact" />
 	</div>
 </div>
 <div class="mx-auto pt-4 w-3/4">
 	<div class="rounded border-2 border-primary-500 p-2">
-		<h3 class="text-primary-500 text-center font-schoolBell text-lg p-1">Data form Fetch</h3>
-		<p class='text-primary-500 text-lg'>Within this table, the data is retrieved from a standalone api endpoint living in <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">Routes</span> folder root.</p>
+		<h3 class="text-primary-500 text-center font-schoolBell text-lg p-1">Data from Fetch</h3>
+		<p class="text-primary-500 text-lg">
+			Within this table, the data is retrieved from a standalone api endpoint living in <span
+				class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">Routes</span> folder root.
+		</p>
 		<ul class="text-primary-500 m-6">
-			<li class="text-primary-500 m-2"> - Initialize a first empty variable which will receives the fetch result later.</li>
-			<li class="text-primary-500 m-2"> - Then, initialize a second variable, being reactive <span class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">$:</span> and assigned to the first one with any operations (map, filter etc ...). This variable will be used in the Ui.</li>
-			<li class="text-primary-500 m-2"> - Secondaly, write a fetch function that populate that variable.</li>
-			<li class="text-primary-500 m-2"> - Finally, Make a fetch call wherever you want to. See following  <span class="bg-success-200 text-success-800 font-schoolBell rounded p-1">Fetch button</span> for instance.</li>
+			<li class="text-primary-500 m-2">
+				- Initialize a first empty variable which will receives the fetch result later.
+			</li>
+			<li class="text-primary-500 m-2">
+				- Then, initialize a second variable, being reactive <span
+					class="bg-primary-200 text-primary-600 font-schoolBell rounded p-1">$:</span> and assigned
+				to the first one with any operations (map, filter etc ...). This variable will be used in the
+				Ui.
+			</li>
+			<li class="text-primary-500 m-2">
+				- Secondaly, write a fetch function that populate that variable.
+			</li>
+			<li class="text-primary-500 m-2">
+				- Finally, Make a fetch call wherever you want to. See following <span
+					class="bg-success-200 text-success-800 font-schoolBell rounded p-1">Fetch button</span> for
+				instance.
+			</li>
 		</ul>
 		{#if FetchFromApi}
 			{#await fetchDataFromApi()}
