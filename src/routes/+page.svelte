59126c04e1c1228b22e13fecd22353203d08b9e5
Vincent Vignali <vincent.vignali@syd.fr>
2023-11-02 13:48:43 +0100

    Add logging to GET request in +server.ts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,70 @@
+<script lang="ts">
+	import { IconTrees } from '@tabler/icons-svelte';
+	const bonDeCommande = { type: 'Bon de Commande', path: '/pdfs/-Bon-de-Commande.pdf' };
+
+	const catalogues = [
+		{ type: 'Cerisiers', path: '/pdfs/-Catalogue-Cerisiers-2023.pdf' },
+		{ type: 'Petits Fruits', path: '/pdfs/-Catalogue-Petit-Fruit-2023.pdf' },
+		{ type: 'Poiriers', path: '/pdfs/-Catalogue-Poiriers-2023.pdf' },
+		{ type: 'Pommiers', path: '/pdfs/-Catalogue-Pommiers-2023.pdf' },
+		{ type: 'Portes Greffes', path: '/pdfs/-Catalogue-Porte-Greffe-2023.pdf' },
+		{ type: 'Pruniers', path: '/pdfs/-Catalogue-Pruniers-2023.pdf' }
+	];
+</script>
+
+
 <div
-	class="w-12/12 h-full mx-auto bg-primary-500 transition-colors duration-300 text-primary-500 flex justify-center gap-10 flex-col">
-	<a
-		href="/app"
-		class="mx-auto btn text-primary-500 bg-white rounded-xl text-ls font-schoolBell tracking-widest h-20 w-1/3 hover:text-3xl"
-		>App</a>
-	<a
-		href="/dev"
-		class="mx-auto btn text-primary-500 bg-white rounded-xl text-ls font-schoolBell tracking-widest h-20 w-1/3 hover:text-3xl"
-		>Dev App</a>
+class="w-12/12 h-full mx-auto bg-primary-500 transition-colors duration-300 text-primary-500 flex justify-center gap-5 flex-col">
+<h1
+	class="font-schoolBell text-2xl text-center text-white">
+	👩‍🌾 👩‍🌾 👨‍🌾
+	<br />
+	Bienvenue au Piton
+</h1>
+<div class="flex flex-col gap-0 text-start mx-auto">
+
+	<p
+	class="font-schoolBell text-md  text-white"
+	>
+	1. Télécharger nos catalogues 2023. 
+</p>
+<p
+class="font-schoolBell text-md  text-white"
+>
+2. Télécharger & compléter un bon de commande.
+</p>
+<p
+class="font-schoolBell text-md  text-white"
+>
+3. Envoyer le à lepitondudehors@protonmail.com. 
+</p>
+<p
+class="font-schoolBell text-md  text-white"
+>
+4. Nous vous contacterons :) 
+</p>
 </div>
+{#each catalogues as catalogue}
+		<a
+			class="mx-auto btn text-primary-500 bg-primary-50 rounded-xl text-sm font-schoolBell tracking-widest h-14 w-80 md:w-1/3 hover:text-lg"
+			download="{catalogue.type}.pdf"
+			href={catalogue.path}>
+			<span class=" ext-primary font-extrabold">{catalogue.type}</span>
+		</a>
+		{/each}
+		<a
+			class="border-success-600 border-4 mx-auto btn text-success-500 bg-white rounded-xl text-sm font-schoolBell tracking-widest h-14 w-80 md:w-1/3 hover:text-lg"
+			download="{bonDeCommande.type}.pdf"
+			href={bonDeCommande.path}>
+			Télécharger le {bonDeCommande.type}
+		</a>
+</div>
+
+<!-- <a
+	href="/app"
+	class="mx-auto btn text-primary-500 bg-white rounded-xl text-ls font-schoolBell tracking-widest h-20 w-1/3 hover:text-3xl"
+	>App</a>
+<a
+	href="/dev"
+	class="mx-auto btn text-primary-500 bg-white rounded-xl text-ls font-schoolBell tracking-widest h-20 w-1/3 hover:text-3xl"
+	>Dev App</a> -->
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -3,7 +3,7 @@ import type { Tree } from '$lib/types/treeType';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
+	console.log('call route racine');
 	const result: Tree[] = await pb.collection('trees').getFullList();
-
 	return new Response(JSON.stringify(result));
 };
