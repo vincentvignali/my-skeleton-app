9f2e24f844d01f09d525039d67c46ad9f2b81bdc
Vincent Vignali <vincent.vignali@syd.fr>
2024-01-05 00:38:21 +0100

    Update styling and add new link in +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <div
-	class="w-12/12 h-full mx-auto bg-primary-500 transition-colors duration-300 text-primary-500 flex justify-center gap-5 flex-col">
+	class="w-12/12 h-full mx-auto bg-primary-500 transition-colors duration-300 text-primary-500 flex justify-center gap-3 flex-col">
 	<h1 class="font-schoolBell text-2xl text-center text-white">
 		👩‍🌾 👩‍🌾 👨‍🌾
 		<br />
@@ -27,6 +27,15 @@
 		</p>
 		<p class="font-schoolBell text-md text-white">4. Nous vous contacterons :)</p>
 	</div>
+	<a
+		class="relative border-warning-500 border-4 mx-auto btn text-warning-600 font-black bg-white rounded-xl text-sm font-schoolBell tracking-widest h-12 w-80 md:w-1/3 hover:text-lg"
+		href="{$page.url}dev/catalogue">
+		<div
+			class="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 border-warning-500 border-dotted border-2 bg-warning-100 text-lg rounded-lg scale-75 outline-8 p-2">
+			Nouveau
+		</div>
+		Essayer notre catablogue en ligne
+	</a>
 	{#each catalogues as catalogue}
 		<a
 			class="mx-auto btn text-primary-500 bg-primary-50 rounded-xl text-sm font-schoolBell tracking-widest h-10 w-80 md:w-1/3 hover:text-lg"
@@ -35,12 +44,6 @@
 			<span class=" ext-primary font-extrabold">{catalogue.type}</span>
 		</a>
 	{/each}
-	<p class="font-schoolBell text-md text-white text-center">Ou</p>
-	<a
-		class="border-warning-600 border-4 mx-auto btn text-warning-700 font-black bg-white rounded-xl text-sm font-schoolBell tracking-widest h-12 w-80 md:w-1/3 hover:text-lg"
-		href="{$page.url}dev/catalogue">
-		Accéder au catablogue en ligne
-	</a>
 	<p class="font-schoolBell text-md text-white text-center">Puis</p>
 	<a
 		class="border-success-600 border-4 mx-auto btn text-success-700 font-black bg-white rounded-xl text-sm font-schoolBell tracking-widest h-12 w-80 md:w-1/3 hover:text-lg"
